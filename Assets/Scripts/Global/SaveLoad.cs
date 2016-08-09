81f1d969087a6eac12a7223723f039c9873b5e8a
mathias Jensen <mathias Jensen>
2016-08-09 10:08:42 +0200

    Changed: SaveLoad script is done

--- Assets/Scripts/Global/SaveLoad.cs
+++ Assets/Scripts/Global/SaveLoad.cs
@@ -4,40 +4,97 @@ using System.Collections;
 public class SaveLoad : MonoBehaviour {
 
 
-    //Unity egen save system. PlayerPref. Eller xml eller JSON. || PlayerPref kræver mindst arbejde. xml eller JSON er nemmere at rette i bagefter?
+    //This is a save/load system. Using Unity PlayerPrefs to create or get data from a place on the local computer.
+    //Settings not done
+    //Host mind not done
 
-    //private array med bools?
-    //get og set for array med bools
-    //private array med host mind bools?
-    //get og set også for det
-    //Settings skal også gemmes
+
+    private bool[] completedLevels;
+    
+    public bool[] CompletedLevels
+    { get{return completedLevels; }set {//Triggered
+            completedLevels = value;
+        }
+    }
+
+    private string[] settings;
+
+    public string[] Settings
+    {
+        get { return settings; }
+        set { settings = value; }
+    }
+
+
+
+    
     
 
 	// Use this for initialization
 	void Start ()
     {
-        //Load();  eller når man klikker på "Continue" knappen i main menu. Så "Start" metoden kan fjernes.
-	
-	}
+
+        settings = new string[] { };
+        completedLevels = new bool[5];// {true,false,false,false,false };
+        
+    }
 	
 
-    //Array med bools, om spilleren har klaret en bane eller ej.
-    //Host mind skal gemmes. 
 
-    //Method to save data
+ /// <summary>
+ /// Save Method, Only for completed levels. Room for change in future.
+ /// </summary>
     public void Save()
     {
-        //En som checker om man har saved. Se om der er sket en fejl?
-        //PlayerPrefsX.SetStringArray("Names", names)
-        // PlayerPrefs.SetInt("LEVELREACHED", InsertNumber);
+
+        try
+        {
+            for (int i = 0; i < completedLevels.Length; i++)
+            {
+                PlayerPrefs.SetInt("Level" + i, completedLevels[i] ? 1 : 0);
+                Debug.Log("Saved : Level" + i + " : " + completedLevels[i]);
+            }
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("SAVE FAILED: DANGER");
+        }
+
+        
+
     }
 
 
-    //Method to load data
+    /// <summary>
+    /// LOAd methoD, also only for completed atm. 
+    /// </summary>
     public void Load()
     {
-        // Skal køres hvis der er noget i den. Ellers ligger den default values ind. Ved bare at køre save..
-        // LevelReached = PlayerPrefs.GetInt("LEVELREACHED");
-        // something = PlayerPrefsX.GetStringArray("Names");
+
+        try
+        {
+            for (int i = 0; i < completedLevels.Length; i++)
+            {
+                if (PlayerPrefs.HasKey("Level" + i))
+                {
+                    if (PlayerPrefs.GetInt("Level" + i) == 0)
+                    {
+                        completedLevels[i] = false;
+                    }
+                    else
+                    {
+                        completedLevels[i] = true;
+                    }
+                    Debug.Log("Loaded : Level" + i + " : " + completedLevels[i]);
+                }
+
+            }
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("LOADED FAILED: DANGER");
+        }
+
+        
     }
 }
