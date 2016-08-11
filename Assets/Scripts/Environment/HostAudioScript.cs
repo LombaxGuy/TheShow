9805290eb54068d40016e6ed8e8c528b3d73e03f
Thorbjorn <superthorbjorn@gmail.com>
2016-08-11 14:56:38 +0200

    Added Most audio script

---
+++ Assets/Scripts/Environment/HostAudioScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class HostAudioScript : MonoBehaviour {
+
+    private static AudioSource audioPLayer;
+    private AudioClip sip;
+
+    private string audiod;
+    
+
+	// Use this for initialization
+	void Start () {
+        audioPLayer = GetComponent<AudioSource>();
+        
+        sip = (AudioClip)Resources.Load("Assets/Resources/TV_Theme160");
+        sip = Resources.Load<AudioClip>("Assets/Resources/TV_Theme160");
+        sip = Resources.Load("Resources/TV_Theme160") as AudioClip;
+        Debug.Log(sip);
+        audioPLayer.clip = sip;
+        //PlayChosenClip(sip);
+        audioPLayer.PlayOneShot(sip);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    public static void PlayChosenClip(AudioClip clip)
+    {
+              audioPLayer.PlayOneShot(clip);
+    }
+}
