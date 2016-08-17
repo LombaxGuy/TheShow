02b433b96050bf8575e8139dff80d276f4e6b22f
Thorbjorn <superthorbjorn@gmail.com>
2016-08-17 16:17:33 +0200

    Added: MapTest scripts Changed: TestMap

---
+++ Assets/Scripts/MapTests/Pusher.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+
+    private float maxPosition;
+    private float minPosition;
+    private float currentPossition;
+    private bool state;
+    private float speed = 1000;
+
+
+    [SerializeField]
+    private float cooldown = 2;
+    [SerializeField]
+    private float wait;
+    [SerializeField]
+    private float offset;
+    private float waitOffset;
+
+    private Rigidbody rig;
+    private Vector3 dimentions;
+
+	// Use this for initialization
+	void Start () {
+        rig = gameObject.GetComponent<Rigidbody>();
+        minPosition = gameObject.transform.position.x;
+        dimentions = gameObject.GetComponent<Renderer>().bounds.size;
+        maxPosition = minPosition + dimentions.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentPossition = gameObject.transform.position.x;
+        waitOffset = waitOffset + 1 * Time.deltaTime;
+        wait = wait + 1 * Time.deltaTime;
+
+        if (offset <= waitOffset)
+        {     
+            forward();
+            backward();
+        }
+    }
+
+    private void forward()
+    {
+        if (currentPossition <= maxPosition && !state && cooldown <= wait)
+        {
+            if (rig.velocity.x < 100)
+            {
+                //rig.AddForce(Vector3.right * speed * Time.deltaTime, ForceMode.VelocityChange);
+                rig.velocity = new Vector3(speed * Time.deltaTime,0,0);
+               
+            }
+        }
+
+            if (currentPossition >= maxPosition && state == false && cooldown <= wait)
+            {
+                state = true;
+                rig.velocity = new Vector3(0, 0, 0);
+                //Debug.Log("Hit max");
+                wait = 0;
+            
+            }
+    }
+    private void backward()
+    {
+        if (currentPossition >= minPosition && state == true && cooldown <= wait)
+        {
+            if (rig.velocity.x < 100)
+            {
+                //rig.AddForce(Vector3.left * speed * Time.deltaTime, ForceMode.VelocityChange);
+                rig.velocity = new Vector3(-speed * Time.deltaTime, 0, 0);
+            }
+
+        }
+        if (currentPossition <= minPosition && state == true && cooldown <= wait)
+        {
+            state = false;
+            rig.velocity = new Vector3(0, 0, 0);
+            //Debug.Log("hit min");
+            wait = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MapTests/RotatorAntiRigid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatorAntiRigid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.Rotate(1,0,0);
+
+	}
+}
