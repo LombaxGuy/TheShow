765a888d398bba078a7ed3f03001b2d4b5e36b73
mathias Jensen <mathias Jensen>
2016-08-19 15:05:25 +0200

    Added: Sounds under test MusicRecord

---
+++ Assets/Scripts/Environment/GameObjectPositionResetTemplate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectPositionResetTemplate : MonoBehaviour {
+
+
+    /// <summary>
+    /// This is a template script for GameObjectPositionReset.
+    /// When the Player dies, Name will be called.
+    /// </summary>
+
+    void OnEnable()
+    {
+        GameObjectPositionReset.resetObjects += Name;
+    }
+
+    void OnDisable()
+    {
+        GameObjectPositionReset.resetObjects -= Name;
+    }
+
+    void Name()
+    {
+        Debug.Log("Called from GameObjectPositionReset");
+    }
+}
