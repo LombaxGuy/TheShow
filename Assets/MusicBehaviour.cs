32fa2a71a7703d7e6a9f0d0fdc924fa32a4b9a76
mathias Jensen <mathias Jensen>
2016-08-22 15:32:38 +0200

    Added: MusicBehaviour Changed: MusicRecord script ConveyorBeltAdvanced script LevelOne scene InteractableObjectComponenet minor text fix

---
+++ Assets/MusicBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(InteractableObjectComponent))]
+public class MusicBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private Camera playerCam;
+
+    [SerializeField]
+    private GameObject cam;
+
+    [SerializeField]
+    private bool musicMode = false;
+
+    public Camera PlayerCam
+    {
+        get { return playerCam; }
+    }
+
+    public Camera Cam
+    {
+        get { return Cam; }
+    }
+
+    // The script containing the delegate
+    InteractableObjectComponent interactableObjectComponent;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the script
+        interactableObjectComponent = GetComponent<InteractableObjectComponent>();
+
+        // If the component exists on the object we assign a behaviour to the delegate in the script component.
+        if (interactableObjectComponent != null)
+        {
+            interactableObjectComponent.behaviourDelegate = ThisSpecificBehaviour;
+            Debug.Log("Music Behaviour assigned to the BehaviourDelegate.");
+        }
+        else
+        {
+            Debug.Log("No 'InteractableObjectComponent' was found!");
+        }
+    }
+
+    /// <summary>
+    /// Defines the behaviour of the object when it is interacted with.
+    /// </summary>
+    private void ThisSpecificBehaviour()
+    {
+        if(playerCam.enabled == true)
+        {
+            if (musicMode == false)
+            {
+                playerCam.GetComponent<Transform>().LookAt(transform);
+
+                GetComponent<MusicRecord>().PlayerClicked = !GetComponent<MusicRecord>().PlayerClicked;
+            }
+            else
+            {
+                cam.SetActive(!cam.activeSelf);
+                playerCam.enabled = !playerCam.enabled;
+                GetComponent<MusicRecord>().PlayerClicked = !GetComponent<MusicRecord>().PlayerClicked;
+            }
+        }
+        
+    }
+
+    public void GetOut()
+    {
+        cam.SetActive(!cam.activeSelf);
+        playerCam.enabled = !playerCam.enabled;
+        GetComponent<MusicRecord>().PlayerClicked = !GetComponent<MusicRecord>().PlayerClicked;
+    }
+}
--- Assets/Scripts/Environment/Interactable Objects/InteractableObjectComponent.cs
+++ Assets/Scripts/Environment/Interactable Objects/InteractableObjectComponent.cs
@@ -1,4 +1,4 @@
-﻿// This script should be attached to all interactable objects (Tagged objects with "Interactable"). Interactable objects are things like doors and buttons and not things that can be picked up.
+﻿// This script should be attached to all interactable objects (Layered objects with "Interactable"). Interactable objects are things like doors and buttons and not things that can be picked up.
 // Along with this script another script which defines the behaviour of the object when it is interacted with must also be pressent for anything to happen. 
 // The 'Behaviour' script mentioned above must set the behaviourDelegate in this script to a method which matches the delegate signature.
 // 
