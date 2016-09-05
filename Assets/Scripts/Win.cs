d3f1c4a940503ca83241426aff995cd3eadcc141
Thorbjorn <superthorbjorn@gmail.com>
2016-09-05 15:27:13 +0200

    Added: Win script Ny moving platform prefab Chanhed: turtorial_!

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+
+    public GameObject wola;
+    public GameObject wola2;
+    public GameObject wola3;
+    public GameObject wola4;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        wola.SetActive(true);
+        wola2.SetActive(true);
+        wola3.SetActive(true);
+        wola4.SetActive(true);
+    }
+}
