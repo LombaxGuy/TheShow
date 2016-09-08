75ec0c8557c142a43c819325e1cd3f1633b42806
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2016-09-08 08:50:23 +0200

    Changed: Begun work on the video settings menu

---
+++ Assets/Scripts/Global/Menus/NumberBoxToSliderValue.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberBoxToSliderValue : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    [SerializeField]
+    private InputField numberBox;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void UpdateSlider()
+    {
+        int value = 0;
+        int.TryParse(numberBox.text, out value);
+
+        value = value > 1 ? (1) : (value);
+
+        slider.value = value;
+    }
+
+    public void UpdateNumberBox()
+    {
+
+    }
+}
