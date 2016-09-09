4664effff5b0b5976e91bce4e10d6ad434683d2d
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2016-09-09 15:02:43 +0200

    Added: Functional Vsync setting

---
+++ Assets/Scripts/Global/Menus/Video Settings/VsyncSetting.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VsyncSetting : MonoBehaviour
+{
+    private Toggle vSyncToggle;
+
+    [SerializeField]
+    private bool defaultVSyncSetting = false;
+
+    private bool currentSetting;
+
+    /// <summary>
+    /// Subscribes to events on awake
+    /// </summary>
+    private void Awake()
+    {
+        EventManager.OnCheckForSettingChanges += OnCheckForSettingChanges;
+        EventManager.OnApplySettingChanges += OnApplySettingChanges;
+        EventManager.OnResetSettings += OnResetSettings;
+        EventManager.OnResetToDefaultSettings += OnResetToDefaultSettings;
+    }
+
+    /// <summary>
+    /// Unsubscribes from events on destroy.
+    /// </summary>
+    private void OnDestroy()
+    {
+        EventManager.OnCheckForSettingChanges -= OnCheckForSettingChanges;
+        EventManager.OnApplySettingChanges -= OnApplySettingChanges;
+        EventManager.OnResetSettings -= OnResetSettings;
+        EventManager.OnResetToDefaultSettings -= OnResetToDefaultSettings;
+    }
+
+    private void Start()
+    {
+        vSyncToggle = GetComponent<Toggle>();
+    }
+
+    private void OnCheckForSettingChanges()
+    {
+        if (vSyncToggle.isOn && QualitySettings.vSyncCount == 0)
+        {
+            EventManager.RaiseOnSettingsChanged();
+        }
+    }
+
+    private void OnApplySettingChanges()
+    {
+        if (vSyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+
+    private void OnResetSettings()
+    {
+        if (QualitySettings.vSyncCount == 0)
+        {
+            vSyncToggle.isOn = false;
+        }
+        else
+        {
+            vSyncToggle.isOn = true;
+        }
+        
+    }
+
+    private void OnResetToDefaultSettings()
+    {
+        vSyncToggle.isOn = defaultVSyncSetting;
+    }
+}
