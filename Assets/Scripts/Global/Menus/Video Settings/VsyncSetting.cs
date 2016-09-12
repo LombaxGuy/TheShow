6311515d923559129ed88c94cc989a4bac14a982
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2016-09-12 10:29:42 +0200

    Changed: Added coments and functionality to the VSync setting

--- Assets/Scripts/Global/Menus/Video Settings/VsyncSetting.cs
+++ Assets/Scripts/Global/Menus/Video Settings/VsyncSetting.cs
@@ -9,8 +9,6 @@ public class VsyncSetting : MonoBehaviour
     [SerializeField]
     private bool defaultVSyncSetting = false;
 
-    private bool currentSetting;
-
     /// <summary>
     /// Subscribes to events on awake
     /// </summary>
@@ -38,41 +36,64 @@ public class VsyncSetting : MonoBehaviour
         vSyncToggle = GetComponent<Toggle>();
     }
 
+    /// <summary>
+    /// Runs when the OnCheckForSettingChanges event is raised
+    /// </summary>
     private void OnCheckForSettingChanges()
     {
+        // If the vSync setting is toggled to 'On' and the current setting is off...
         if (vSyncToggle.isOn && QualitySettings.vSyncCount == 0)
         {
+            //... raise the following event.
             EventManager.RaiseOnSettingsChanged();
         }
     }
 
+    /// <summary>
+    /// Runs when the OnApplySettingChanges event is raised
+    /// </summary>
     private void OnApplySettingChanges()
     {
+        // If the vSync setting is set to on...
         if (vSyncToggle.isOn)
         {
+            //... set the vSyncCount to 1
             QualitySettings.vSyncCount = 1;
         }
+        // Otherwise...
         else
         {
+            //... set the vSyncCount to 0
             QualitySettings.vSyncCount = 0;
         }
     }
 
+    /// <summary>
+    /// Runs when the OnReset event is raised
+    /// </summary>
     private void OnResetSettings()
     {
+        // If vSync is currently turned off...
         if (QualitySettings.vSyncCount == 0)
         {
+            //... set the menu toggle to 'Off'.
             vSyncToggle.isOn = false;
         }
+        // Otherwise...
         else
         {
+            //... set the menu toggle to 'On'.
             vSyncToggle.isOn = true;
         }
         
     }
 
+    /// <summary>
+    /// Runs when the OnResetToDefaultSettings event is raised
+    /// </summary>
     private void OnResetToDefaultSettings()
     {
+        // Resets the vSync setting to the default setting.
         vSyncToggle.isOn = defaultVSyncSetting;
     }
 }
