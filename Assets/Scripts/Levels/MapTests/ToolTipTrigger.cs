b786ad47d0f1b29d40e2b9787a56aa1c54fb566b
Thorbjorn <superthorbjorn@gmail.com>
2016-09-12 16:32:46 +0200

    Added: Room prefab Turtorial_0 Changed: subtitle collection

---
+++ Assets/Scripts/Levels/MapTests/ToolTipTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolTipTrigger : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject manager;
+
+    [SerializeField]
+    private string messege;
+
+    // Use this for initialization
+    void Start () {
+        manager = GameObject.Find("WorldManager");
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //manager.GetComponent<Tooltip>().DisplayTooltipForSeconds(messege, 5);
+            IntroSequence.lightEntered = true;
+            IntroSequence.timeInSeconds = IntroSequence.timer;
+            gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Levels/MapTests/Turtorial_0/Door.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    [SerializeField]
+    private GameObject item;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+            item.SetActive(false);
+        }
+        
+    }
+}
