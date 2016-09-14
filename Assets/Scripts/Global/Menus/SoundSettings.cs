c9d794edd99b0cd2ad4edd0756215248a6a5f511
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2016-09-14 09:42:03 +0200

    Changed: Fixed some issues with sound settings

--- Assets/Scripts/Global/Menus/SoundSettings.cs
+++ Assets/Scripts/Global/Menus/SoundSettings.cs
@@ -8,9 +8,13 @@ public class SoundSettings : MonoBehaviour
     [SerializeField]
     private AudioMixer[] mixer;
 
-    private bool hasChanged;
+    [SerializeField]
+    private float defaultVolume = 1;
+
     private bool changePersists;
-    private float[] saveValues = new float[4];
+    private float[] currentValues = new float[4];
+    private float[] oldValues = new float[4];
+
     private float[] values = new float[4];
 
     public AudioMixer[] Mixer
@@ -25,8 +29,9 @@ public class SoundSettings : MonoBehaviour
         EventManager.OnCheckForSettingChanges += OnCheckForSettingChanges;
         EventManager.OnResetSettings += OnResetSettings;
         EventManager.OnResetToDefaultSettings += OnResetToDefaultSettings;
+        EventManager.OnApplySettingChanges += OnApplySettingChanges;
         EventManager.OnSavePref += SaveSoundSliders;
-        EventManager.OnLoadPref += LoadSoundSliders;
+        EventManager.OnLoadPref += LoadOnStartUp;
     }
 
     /// <summary>
@@ -37,9 +42,11 @@ public class SoundSettings : MonoBehaviour
         EventManager.OnCheckForSettingChanges -= OnCheckForSettingChanges;
         EventManager.OnResetSettings -= OnResetSettings;
         EventManager.OnResetToDefaultSettings -= OnResetToDefaultSettings;
+        EventManager.OnApplySettingChanges -= OnApplySettingChanges;
         EventManager.OnSavePref -= SaveSoundSliders;
-        EventManager.OnLoadPref -= LoadSoundSliders;
+        EventManager.OnLoadPref -= LoadOnStartUp;
     }
+
     /// <summary>
     /// Changes the volume of the master channel.
     /// </summary>
@@ -47,8 +54,8 @@ public class SoundSettings : MonoBehaviour
     public void ChangeVolumeMaster(float value)
     {
         Mixer[0].SetFloat("masterVol", LinearToDecibel(value));
-        hasChanged = true;
-        saveValues[0] = value;
+        //EventManager.RaiseOnSettingsChanged();
+        currentValues[0] = value;
     }
 
     /// <summary>
@@ -58,8 +65,8 @@ public class SoundSettings : MonoBehaviour
     public void ChangeVolumeMusic(float value)
     {
         Mixer[1].SetFloat("musicVol", LinearToDecibel(value));
-        hasChanged = true;
-        saveValues[1] = value;
+        //EventManager.RaiseOnSettingsChanged();
+        currentValues[1] = value;
     }
 
     /// <summary>
@@ -69,8 +76,8 @@ public class SoundSettings : MonoBehaviour
     public void ChangeVolumeFX(float value)
     {
         Mixer[2].SetFloat("fxVol", LinearToDecibel(value));
-        hasChanged = true;
-        saveValues[2] = value;
+        //EventManager.RaiseOnSettingsChanged();
+        currentValues[2] = value;
     }
 
     /// <summary>
@@ -80,9 +87,8 @@ public class SoundSettings : MonoBehaviour
     public void ChangeVolumeVoice(float value)
     {
         Mixer[3].SetFloat("voiceVol", LinearToDecibel(value));
-        hasChanged = true;
-        saveValues[3] = value;
-        Debug.Log(saveValues[3]);
+        EventManager.RaiseOnSettingsChanged();
+        currentValues[3] = value;
     }
 
     /// <summary>
@@ -108,60 +114,50 @@ public class SoundSettings : MonoBehaviour
 
     public void OnCheckForSettingChanges()
     {
-        float[] prefValues = SaveLoad.LoadSoundPrefs();
-        changePersists = false;
-        if (hasChanged)
+        for (int i = 0; i < oldValues.Length; i++)
         {
-            for (int i = 0; i < prefValues.Length; i++)
-            {
-                Debug.Log(prefValues[i]);
-                Debug.Log(saveValues[i]);
-                Debug.Log(changePersists);
-                if (prefValues[i] != saveValues[i])
-                {
-                    changePersists = true;
-                }
+            Debug.Log("Old " + i + ": " + oldValues[i]);
+            Debug.Log("New " + i + ": " + currentValues[i]);
 
+            if (oldValues[i] != currentValues[i])
+            {
+                //changePersists = true;
+                EventManager.RaiseOnSettingsChanged();
             }
-        }
-        if (changePersists)
-        {
-            EventManager.RaiseOnSettingsChanged();
         }
     }
 
-
     private void OnResetSettings()
     {
-        //Sets the values variable to be equal to the array returned in the LoadPrefs method.
-
-        //Sets the slider values to be equal to the value variable and deactivates the menu
-
-        transform.FindChild("MasterSlider").GetComponent<Slider>().value = values[0];
-        transform.FindChild("MusicSlider").GetComponent<Slider>().value = values[1];
-        transform.FindChild("FXSlider").GetComponent<Slider>().value = values[2];
-        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = values[3];
+        transform.FindChild("MasterSlider").GetComponent<Slider>().value = oldValues[0];
+        transform.FindChild("MusicSlider").GetComponent<Slider>().value = oldValues[1];
+        transform.FindChild("FXSlider").GetComponent<Slider>().value = oldValues[2];
+        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = oldValues[3];
     }
 
     private void OnResetToDefaultSettings()
     {
-        transform.FindChild("MasterSlider").GetComponent<Slider>().value = 1;
-        transform.FindChild("MusicSlider").GetComponent<Slider>().value = 1;
-        transform.FindChild("FXSlider").GetComponent<Slider>().value = 1;
-        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = 1;
+        transform.FindChild("MasterSlider").GetComponent<Slider>().value = defaultVolume;
+        transform.FindChild("MusicSlider").GetComponent<Slider>().value = defaultVolume;
+        transform.FindChild("FXSlider").GetComponent<Slider>().value = defaultVolume;
+        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = defaultVolume;
     }
 
-    private void LoadSoundSliders()
+    private void OnApplySettingChanges()
+    {
+        oldValues = currentValues;
+    }
+
+    private void LoadOnStartUp()
     {
         //Sets the values variable to be equal to the array returned in the LoadPrefs method.
-        saveValues = SaveLoad.LoadSoundPrefs();
+        oldValues = SaveLoad.LoadSoundPrefs();
 
         //Sets the slider values to be equal to the value variable and deactivates the menu
-
-        transform.FindChild("MasterSlider").GetComponent<Slider>().value = saveValues[0];
-        transform.FindChild("MusicSlider").GetComponent<Slider>().value = saveValues[1];
-        transform.FindChild("FXSlider").GetComponent<Slider>().value = saveValues[2];
-        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = saveValues[3];
+        transform.FindChild("MasterSlider").GetComponent<Slider>().value = oldValues[0];
+        transform.FindChild("MusicSlider").GetComponent<Slider>().value = oldValues[1];
+        transform.FindChild("FXSlider").GetComponent<Slider>().value = oldValues[2];
+        transform.FindChild("VoiceSlider").GetComponent<Slider>().value = oldValues[3];
     }
 
     /// <summary>
@@ -170,14 +166,6 @@ public class SoundSettings : MonoBehaviour
     private void SaveSoundSliders()
     {
         //Saves our slider values in the value variable
-        saveValues[0] = transform.FindChild("MasterSlider").GetComponent<Slider>().value;
-        saveValues[1] = transform.FindChild("MusicSlider").GetComponent<Slider>().value;
-        saveValues[2] = transform.FindChild("FXSlider").GetComponent<Slider>().value;
-        saveValues[3] = transform.FindChild("VoiceSlider").GetComponent<Slider>().value;
-
-
-        SaveLoad.SaveSoundPrefs(saveValues[0], saveValues[1], saveValues[2], saveValues[3]);
-
-        values = saveValues;
+        SaveLoad.SaveSoundPrefs(currentValues[0], currentValues[1], currentValues[2], currentValues[3]);
     }
 }
