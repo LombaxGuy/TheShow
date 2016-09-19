e054518c6ac541a65890b2e35f66d57216315945
mathias Jensen <mathias Jensen>
2016-09-19 15:27:05 +0200

    Changed> ConveyorBeltLevel Added> GridPuzzle and CellBehaciour script, test and building Grid puzzle

---
+++ Assets/Scripts/Levels/MapTests/CellBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private bool deathCell = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(deathCell == true)
+        {
+            if(other.transform.tag == "Player")
+            {
+                other.transform.GetComponent<PlayerRespawn>().Kill();
+            }
+        }
+    }
+}
