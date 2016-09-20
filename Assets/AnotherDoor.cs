1ade16310da85a7780365fb15e846060ca959b0b
mathias Jensen <mathias Jensen>
2016-09-20 12:42:13 +0200

    Changed: ConveyorBelt scene Added: CellBehaciour AnotherDoor

---
+++ Assets/AnotherDoor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            OpenDoor();
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            CloseDoor();
+        }
+
+	}
+
+    public void OpenDoor()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+    }
+
+    public void CloseDoor()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z);
+    }
+}
--- Assets/Scripts/Levels/MapTests/CellBehaviour.cs
+++ Assets/Scripts/Levels/MapTests/CellBehaviour.cs
@@ -6,23 +6,63 @@ public class CellBehaviour : MonoBehaviour {
     [SerializeField]
     private bool deathCell = false;
 
+    [SerializeField]
+    private GameObject[] doors;
+
+    float timer = 0;
+
+    private GameObject player;
+
+    private bool playerIsInside = false;
+
+    private bool roomCleared = false;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+
+        if(playerIsInside == true && timer <= 0)
+        {
+            DeathOrOpenDoors();
+        }
 	}
 
-    void OnCollisionEnter(Collision other)
+    private void DeathOrOpenDoors()
     {
         if(deathCell == true)
         {
-            if(other.transform.tag == "Player")
+            player.transform.parent.transform.GetComponent<PlayerRespawn>().Kill();
+        }
+        else
+        {
+            //Play Sound
+            for (int i = 0; i < doors.Length; i++)
             {
-                other.transform.GetComponent<PlayerRespawn>().Kill();
+                doors[i].GetComponent<AnotherDoor>().OpenDoor();
+            }
+        }
+        roomCleared = true;
+        playerIsInside = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I WAS HERE" + other.tag);
+        if (other.transform.parent.tag == "Player" && playerIsInside == false && roomCleared == false)
+        {
+            timer = 2;
+            playerIsInside = true;
+            player = other.gameObject;
+            for (int i = 0; i < doors.Length; i++)
+            {
+                doors[i].GetComponent<AnotherDoor>().CloseDoor();
             }
         }
     }
