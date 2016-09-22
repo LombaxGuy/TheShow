2bd636a9d79ee308cbceea351fc1ebb1d7cfc4f1
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2016-09-22 13:25:29 +0200

    Changed: Added comments to a few setting scripts.

--- Assets/Scripts/Global/Menus/Sound Settings/SubtitleSetting.cs
+++ Assets/Scripts/Global/Menus/Sound Settings/SubtitleSetting.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -7,6 +8,7 @@ public class SubtitleSetting : MonoBehaviour
     private Toggle subtitleToggle;
 
     [SerializeField]
+    [Tooltip("The default setting for subtitles. If true subtitles are enabled by default.")]
     private bool defaultSubtitleSetting = false;
 
     private bool subtitlesEnabled;
@@ -29,6 +31,7 @@ public class SubtitleSetting : MonoBehaviour
         EventManager.OnSavePref += OnSavePref;
         EventManager.OnLoadPref += OnLoadPref;
 
+        // Gets the toggle component
         subtitleToggle = GetComponent<Toggle>();
     }
 
@@ -45,52 +48,70 @@ public class SubtitleSetting : MonoBehaviour
         EventManager.OnLoadPref -= OnLoadPref;
     }
 
+    /// <summary>
+    /// Runs when the OnCheckForSettingsChanges event is called.
+    /// </summary>
     private void OnCheckForSettingChanges()
     {
+        // If the current setting is not equals the meun toggle
         if (subtitlesEnabled != subtitleToggle.isOn)
         {
             EventManager.RaiseOnSettingsChanged();
         }
     }
 
+    /// <summary>
+    /// Runs when the OnApplySettingChanges event is called.
+    /// </summary>
     private void OnApplySettingChanges()
     {
+        // Set the current setting to whatever the toggle is set to
         subtitlesEnabled = subtitleToggle.isOn;
     }
 
+    /// <summary>
+    /// Runs when the OnResetSettings event is called.
+    /// </summary>
     private void OnResetSettings()
     {
+        // Set the toggle to whatever the current setting is set to
         subtitleToggle.isOn = subtitlesEnabled;
     }
 
+    /// <summary>
+    /// Runs when the OnResetToDefaultSettings event is called.
+    /// </summary>
     private void OnResetToDefaultSettings()
     {
+        // Set the toggle to the default setting and set the current setting to the default setting
         subtitleToggle.isOn = defaultSubtitleSetting;
         subtitlesEnabled = defaultSubtitleSetting;
     }
 
+    /// <summary>
+    /// Runs when the OnSavePref event is called.
+    /// </summary>
     private void OnSavePref()
     {
+        // Saves the variable 'subtitlesEnabled'
         SaveLoad.SaveSettings("Subtitles", subtitlesEnabled);
     }
 
+    /// <summary>
+    /// Runs when the OnLoadPref event is called.
+    /// </summary>
     private void OnLoadPref()
     {
+        // Loads the variable saves as 'Subtitles'
         int savedSubtitle = SaveLoad.LoadSettingInt("Subtitles");
 
-        if(savedSubtitle != -1)
+        // If the load is succesful the variables are set
+        if (savedSubtitle != -1)
         {
-            if(savedSubtitle == 0)
-            {
-                subtitlesEnabled = false;
-                subtitleToggle.isOn = false;
-            }
-            else
-            {
-                subtitlesEnabled = true;
-                subtitleToggle.isOn = true;
-            }
+            subtitlesEnabled = Convert.ToBoolean(savedSubtitle);
+            subtitleToggle.isOn = Convert.ToBoolean(savedSubtitle);
         }
+        // Else the variables are set to the default value
         else
         {
             subtitlesEnabled = defaultSubtitleSetting;
--- Assets/Scripts/Global/Menus/Video Settings/ShadowQualitySetting.cs
+++ Assets/Scripts/Global/Menus/Video Settings/ShadowQualitySetting.cs
@@ -7,6 +7,7 @@ public class ShadowQualitySetting : MonoBehaviour
     private enum Quality { Disabled, Low, Medium, High, VeryHigh };
 
     [SerializeField]
+    [Tooltip("The DropDown-menu used to specify shadow quality.")]
     private Dropdown shadowQualityDD;
 
     /// <summary>
@@ -37,16 +38,22 @@ public class ShadowQualitySetting : MonoBehaviour
 
     private void Start()
     {
+        // Sets the value of the dropdown menu to the currently selected value
         shadowQualityDD.value = GetCurrentSettingValue();
     }
 
+    /// <summary>
+    /// Compares the current settings and the selected settings and return a bool based on whether or not the settings has changed.
+    /// </summary>
+    /// <returns>Returns true if the settings has changed.</returns>
     private bool CompareShadowSettings()
     {
         bool hasSettingsChanged = false;
 
-        switch (shadowQualityDD.options[shadowQualityDD.value].text)
+        // Switches on the value of the dropdown menu
+        switch (shadowQualityDD.value)
         {
-            case "Disabled":
+            case 0:
                 if (QualitySettings.shadowDistance == 0 && QualitySettings.shadowResolution == ShadowResolution.Low)
                 {
                     hasSettingsChanged = false;
@@ -57,7 +64,7 @@ public class ShadowQualitySetting : MonoBehaviour
                 }
                 break;
 
-            case "Low":
+            case 1:
                 if (QualitySettings.shadowDistance != 0 && QualitySettings.shadowResolution == ShadowResolution.Low)
                 {
                     hasSettingsChanged = false;
@@ -68,15 +75,15 @@ public class ShadowQualitySetting : MonoBehaviour
                 }
                 break;
 
-            case "Medium":
+            case 2:
                 hasSettingsChanged = QualitySettings.shadowResolution == ShadowResolution.Medium ? (false) : (true);
                 break;
 
-            case "High":
+            case 3:
                 hasSettingsChanged = QualitySettings.shadowResolution == ShadowResolution.High ? (false) : (true);
                 break;
 
-            case "Very High":
+            case 4:
                 hasSettingsChanged = QualitySettings.shadowResolution == ShadowResolution.VeryHigh ? (false) : (true);
                 break;
         }
@@ -84,18 +91,28 @@ public class ShadowQualitySetting : MonoBehaviour
         return hasSettingsChanged;
     }
 
+    /// <summary>
+    /// Returns the value of the current setting in the dropdown menu.
+    /// </summary>
+    /// <returns>The value of the currnet setting.</returns>
     private int GetCurrentSettingValue()
     {
         return (int)GetCurrentShadowQualitySetting();
     }
 
+    /// <summary>
+    /// Returns the current Quality of the shadows.
+    /// </summary>
+    /// <returns>The curren Quality of the shadows.</returns>
     private Quality GetCurrentShadowQualitySetting()
     {
         Quality currentSetting = Quality.Disabled;
 
+        // Switches on the QualitySettings.shadowResolution
         switch (QualitySettings.shadowResolution)
         {
             case ShadowResolution.Low:
+                // If the shadowDistance is 0 and the ShadowResolution is Low.
                 if (QualitySettings.shadowDistance == 0)
                 {
                     currentSetting = Quality.Disabled;
@@ -121,48 +138,55 @@ public class ShadowQualitySetting : MonoBehaviour
         return currentSetting;
     }
 
+    /// <summary>
+    /// Runs when the OnCheckForSettingChanges event is raised
+    /// </summary>
     private void OnCheckForSettingChanges()
     {
+        // If the settings has changed the event OnSettingsChanged is raised.
         if (CompareShadowSettings())
         {
             EventManager.RaiseOnSettingsChanged();
         }
     }
 
+    /// <summary>
+    /// Runs when the OnApplySettingChanges event is raised
+    /// </summary>
     private void OnApplySettingChanges()
     {
-        // Sets the texture quality according to the name of the option in the dropdown menu.
-        switch (shadowQualityDD.options[shadowQualityDD.value].text)
+        // Switches on the value of the dropdown menu and sets the settings accordingly
+        switch (shadowQualityDD.value)
         {
-            case "Disabled":
+            case 0:
                 QualitySettings.shadowDistance = 0;
                 QualitySettings.shadowResolution = ShadowResolution.Low;
                 QualitySettings.shadowCascades = 0;
                 QualitySettings.shadowProjection = ShadowProjection.StableFit;
                 break;
 
-            case "Low":
+            case 1:
                 QualitySettings.shadowDistance = 50;
                 QualitySettings.shadowResolution = ShadowResolution.Low;
                 QualitySettings.shadowCascades = 0;
                 QualitySettings.shadowProjection = ShadowProjection.StableFit;
                 break;
 
-            case "Medium":
+            case 2:
                 QualitySettings.shadowDistance = 75;
                 QualitySettings.shadowResolution = ShadowResolution.Medium;
                 QualitySettings.shadowCascades = 2;
                 QualitySettings.shadowProjection = ShadowProjection.StableFit;
                 break;
 
-            case "High":
+            case 3:
                 QualitySettings.shadowDistance = 100;
                 QualitySettings.shadowResolution = ShadowResolution.High;
                 QualitySettings.shadowCascades = 4;
                 QualitySettings.shadowProjection = ShadowProjection.StableFit;
                 break;
 
-            case "Very High":
+            case 4:
                 QualitySettings.shadowDistance = 150;
                 QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
                 QualitySettings.shadowCascades = 4;
@@ -171,13 +195,21 @@ public class ShadowQualitySetting : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Runs when the OnResetSettings event is raised
+    /// </summary>
     private void OnResetSettings()
     {
+        // Sets the value of the dropdown menu to the current value
         shadowQualityDD.value = GetCurrentSettingValue();
     }
 
+    /// <summary>
+    /// Runs when the OnResetToDefaultSettings event is raised
+    /// </summary>
     private void OnResetToDefaultSettings()
     {
+        // Sets the settings to their defalult values.
         QualitySettings.shadowDistance = 75;
         QualitySettings.shadowResolution = ShadowResolution.Medium;
         QualitySettings.shadowCascades = 2;
@@ -185,15 +217,24 @@ public class ShadowQualitySetting : MonoBehaviour
         shadowQualityDD.value = 2;
     }
 
+    /// <summary>
+    /// Runs when the OnSavePref event is raised
+    /// </summary>
     private void OnSavePref()
     {
+        // Saves the current setting
         SaveLoad.SaveSettings("shadow", shadowQualityDD.options[shadowQualityDD.value].text);
     }
 
+    /// <summary>
+    /// Runs when the OnLoadPref event is raised
+    /// </summary>
     private void OnLoadPref()
     {
+        // Loads the shadow setting
         string ddSetting = SaveLoad.LoadSettingString("shadow");
 
+        // If the load was sucssfull the settings is set
         if(ddSetting != null)
         {
             switch(ddSetting)
