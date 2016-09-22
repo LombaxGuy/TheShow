63dd7857eb21861649e4da269094c2a9bb75f01f
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2016-09-22 19:32:02 +0200

    Added: Created a fire particle effect
    Changed:
    Minor changes to the electric spark particle effect

---
+++ Assets/Scripts/Misc/Particles/FireParticle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireParticle : MonoBehaviour
+{
+    private Light fireLight;
+
+    [SerializeField]
+    private float changeIntensityInterval = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        fireLight = GetComponent<Light>();
+
+        StartCoroutine(ChangeLightIntensity());
+    }
+
+    private IEnumerator ChangeLightIntensity()
+    {
+        while (true)
+        {
+            fireLight.intensity = Random.Range(6.0f, 7.0f);
+            yield return new WaitForSeconds(changeIntensityInterval);
+        }
+    }
+}
